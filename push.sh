343faaefa4303f19da34206dc25eddd0ac3196b4
Francisco Merino-Casallo <fmerinocasallo@users.noreply.github.com>
2024-08-26 13:18:48 +0200

    Update push.sh
    Fix typo VERSION var.

--- push.sh
+++ push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VESRION='2024-08-23'
+VERSION='2024-08-23'
 
-docker push gnasello/qupath-env:$VESRION
+docker push gnasello/qupath-env:$VERSION
 
-docker tag gnasello/qupath-env:$VESRION gnasello/qupath-env:latest
+docker tag gnasello/qupath-env:$VERSION gnasello/qupath-env:latest
 
 docker push gnasello/qupath-env:latest
